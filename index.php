d4ec930481c215fed18524a21ca3826a60697ae3
haivt <haivt@smartosc.com>
2016-01-19 10:17:24 +0700

    start demo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+  <HEAD>
+    <LINK href="css/styles.css" rel="stylesheet" type="text/css">
+  </HEAD>
+  <BODY>
+    <div class="wrapper">
+		<div class="item">Hello</div>
+		<div class="item">I am gulp</div>
+	</div>
+  </BODY>
+</HTML>
